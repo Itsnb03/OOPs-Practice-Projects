816e6fddec05ae1d94852f78aa61ea1edc7e3f60
Nitin Bharadwaj <nitinbharadwaj47@gmail.com>
2024-09-13 00:43:37 +0530

    learned what is Instance and Class Variables

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 class Employee:
-     def __init__(self,fname,lname,salary):
+     increment = 1.5  # Class variable
+     no_of_employees = 0
+     
+     def __init__(self, fname, lname, salary):
           self.fname = fname
           self.lname = lname
           self.salary = salary
+          Employee.no_of_employees +=1
+
+     def increase(self):
+          self.salary = int(self.salary * Employee.increment)
+print(Employee.no_of_employees)
+nitin = Employee("nitin", "bharadwaj", 44000)
+print(Employee.no_of_employees)
+rohan = Employee("rohan", "das", 44000)
+print(Employee.no_of_employees)
 
 
-nitin = Employee("nitin","bharadwaj",44000)
-rohan = Employee("rohan","das",44000)
 
-print(nitin.fname,rohan.fname)
+#print(nitin.__dict__)
+#itin.increment = 9
+#print(nitin.__dict__)
+
+#print(nitin.salary)
+#nitin.increase()  
+#print(nitin.salary)  
+
+#print(Employee.__dict__)
